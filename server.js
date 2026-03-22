6a8e29a98209e7eeccb010f5d3f3d8d86024e401
Nisarga <1da23cs117.cs@drait.edu.in>
2026-03-22 13:08:25 +0530

    fix email smtp port

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const pool = new Pool({
 
 // ✅ Email transporter (FIXED)
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: 'gmail',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
