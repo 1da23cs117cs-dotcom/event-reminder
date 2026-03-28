d9bbaa02c7b7e7a69bba59baba2d68b32472ec90
Nisarga <1da23cs117.cs@drait.edu.in>
2026-03-28 21:41:17 +0530

    ddsdD

--- server.js
+++ server.js
@@ -208,7 +208,9 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log("🚀 Server running on port", PORT);
-  await createTables(); // 🔥 auto-create tables
+
+  // run in background (no blocking)
+  createTables();
 });
